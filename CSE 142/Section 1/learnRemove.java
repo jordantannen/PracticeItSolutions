a7c33da916883b23abef7b149415a07b973139a1
Jordan Tannenbaum <95385489+jordantannen@users.noreply.github.com>
2022-06-11 15:03:13 -0400

    Update learnRemove.java

--- CSE 142/Section 1/learnRemove.java
+++ CSE 142/Section 1/learnRemove.java
@@ -1,13 +1,5 @@
 public void learnRemove(int index){
-    int[] newArr = new int[size - 1];
-    int j = 0;
-
-    for (int i = 0; i < size; i++){
-        if (i != index){
-            newArr[j] = elementData[i];
-            j++;
-        }
-    }
-    elementData = newArr;
+   for (int i = index; i < size - 1; i++)
+        elementData[i] = elementData[i + 1];
     size--;
 }

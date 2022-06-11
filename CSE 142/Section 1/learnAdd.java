0f8d680381ae7fc91dbe956c938a2a74bc8a487f
Jordan Tannenbaum <95385489+jordantannen@users.noreply.github.com>
2022-06-11 15:06:09 -0400

    Update learnAdd.java

--- CSE 142/Section 1/learnAdd.java
+++ CSE 142/Section 1/learnAdd.java
@@ -1,16 +1,6 @@
 public void learnAdd(int index, int num){
-    int[] newArr = new int[size + 1];
-    int j = 0;
-
-    for (int i = 0; i < size + 1; i++){
-        if (i != index){
-            newArr[i] = elementData[j];
-            j++;
-        }
-        else{
-            newArr[i] = num;
-        }
-    }
-    elementData = newArr;
+    for (int i = size; i > index; i--) 
+        elementData[i] = elementData[i - 1];
+    elementDate[index] = value;
     size++;
 }

fcf9f18288a476d13bae1474ed7234339de29c0d
Jordan Tannenbaum <95385489+jordantannen@users.noreply.github.com>
2022-06-14 22:13:03 +0000

    Used for each in equals()

--- CSE 142/Section 3/equals.java
+++ CSE 142/Section 3/equals.java
@@ -1,11 +1,10 @@
 public boolean equals(Set<Integer> s1, Set<Integer> s2){
 	if (s1.size() != s2.size())
 		return false;
-	
-	Iterator<Integer> itr = s1.iterator();
-	while (itr.hasNext()){
-			if (!s2.contains(itr.next()))
-					return false;
+
+	for (int num : s1){
+		if (!s2.contains(num))
+			return false;
 	}
 	return true;
 }

39d6b5d96f03918ca7b98fec0c1b85b24714fd9e
Jordan Tannenbaum <95385489+jordantannen@users.noreply.github.com>
2022-06-14 22:08:44 +0000

    Update equals()

--- CSE 142/Section 3/equals.java
+++ CSE 142/Section 3/equals.java
@@ -1,8 +1,11 @@
 public boolean equals(Set<Integer> s1, Set<Integer> s2){
+	if (s1.size() != s2.size())
+		return false;
+	
 	Iterator<Integer> itr = s1.iterator();
 	while (itr.hasNext()){
 			if (!s2.contains(itr.next()))
 					return false;
 	}
-	return s1.size() == s2.size();
+	return true;
 }
